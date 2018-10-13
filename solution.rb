ccf88ab63d99be956b67733f84697dce7c2ae00a
Daniel Tobón <daniel18.tobon@gmail.com>
2018-10-13 10:58:37 -0500

    ajustes

--- solution.rb
+++ solution.rb
@@ -1,10 +1,10 @@
 require 'sinatra'
 
  	get '/' do
- 	if (params[:name]==nil || params[:name]=={} || params[:name]=="")
- 		"<h1> hola desconocido! </h1>"
+ 	unless params[:name]
+ 		"<h1> Hola desconocido! </h1>"
  	else
- 		"<h1> hola #{params[:name]}! </h1>"	
+ 		"<h1> Hola #{params[:name]}! </h1>"	
  	end	
 end	
 
