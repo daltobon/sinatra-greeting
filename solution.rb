ea5cd44420592d8125292f58ceb355565b1694a6
Daniel Tobón <daniel18.tobon@gmail.com>
2018-10-13 11:22:44 -0500

    ajustes

--- solution.rb
+++ solution.rb
@@ -1,7 +1,7 @@
 require 'sinatra'
 
- 	get '/makers/:nombre' do
- 	if (:nombre==nil || :nombre=={} || :nombre=="")
+ 	get '/makers/:name' do
+ 	if (:name==nil || :name=={} || :name=="")
  		"<h1> hola desconocido! </h1>"
  	else
  		"<h1> Hola" +"#{params[:nombre]}! </h1>".capitalize	
