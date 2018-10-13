93de162b2f73c95a87724fc9407d27881f2d8f40
Daniel Tobón <daniel18.tobon@gmail.com>
2018-10-13 10:28:12 -0500

    ajustes

--- solution.rb
+++ solution.rb
@@ -1,10 +1,9 @@
 require 'sinatra'
  	get '/' do
  	unless params[:name]
- 		@saludo ='Hola desconocido'
- 		@instrucciones = "Coloca tu nombre en la URL asi: /?name=tu_nombre"
+ 		@saludo ='Hola desconocido!'
  	else
- 		@saludo = "Hola #{params[:name]}"	
+ 		@saludo = "Hola #{params[:name]}!"	
  	end	
 	erb :index	
 end	
